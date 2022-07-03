718d41e68ee6a59158751a686c0ada97301fd5f5
Rogério Fontes <fontestz@gmail.com>
2022-07-03 10:33:58 -0300

    first commit

--- src/main/java/com/codeusingjava/RedisDatabaseApplication.java
+++ src/main/java/com/codeusingjava/RedisDatabaseApplication.java
@@ -3,10 +3,13 @@ package com.codeusingjava;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = RedisAutoConfiguration.class)
 public class RedisDatabaseApplication implements CommandLineRunner {
 
     @Autowired

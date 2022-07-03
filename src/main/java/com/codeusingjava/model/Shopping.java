ba9ea2278ae61ce8f7be64b2944c6180373f3bfe
Rogério Fontes <fontestz@gmail.com>
2022-07-02 23:44:40 -0300

    first commit

---
+++ src/main/java/com/codeusingjava/model/Shopping.java
@@ -0,0 +1,20 @@
+package com.codeusingjava.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("Product")
+public class Shopping implements Serializable {
+    @Id
+    private int id;
+    private String name;
+    private long price;
+}
---
+++ src/main/java/com/codeusingjava/repo/ShoppingDao.java
@@ -0,0 +1,36 @@
+package com.codeusingjava.repo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.codeusingjava.model.Shopping;
+
+import java.util.List;
+
+@Repository
+public class ShoppingDao {
+
+    public static final String HASH_KEY = "Shopping";
+    @Autowired
+    private RedisTemplate template;
+
+    public Shopping save(Shopping shopping){
+        template.opsForHash().put(HASH_KEY,shopping.getId(),shopping);
+        return shopping;
+    }
+
+    public List<Shopping> findAll(){
+        return template.opsForHash().values(HASH_KEY);
+    }
+
+    public Shopping findProductById(int id){
+        return (Shopping) template.opsForHash().get(HASH_KEY,id);
+    }
+
+
+    public String deleteProduct(int id){
+         template.opsForHash().delete(HASH_KEY,id);
+        return "shopping item deleted successfully !!";
+    }
+}

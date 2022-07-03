40924eadd8f2c55e4b3fd2a0ae346a1cc8ecc944
Rogério Fontes <fontestz@gmail.com>
2022-07-03 20:29:58 -0300

    Configuração do Cluster [2]

--- src/main/java/com/codeusingjava/RedisDatabaseApplication.java
+++ src/main/java/com/codeusingjava/RedisDatabaseApplication.java
@@ -1,5 +1,7 @@
 package com.codeusingjava;
 
+import com.codeusingjava.repo.ShoppingDao;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,12 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 
+@Slf4j
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = RedisAutoConfiguration.class)
 public class RedisDatabaseApplication implements CommandLineRunner {
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private ShoppingDao shoppingDao;
 
     public static void main(String[] args) {
         SpringApplication.run(RedisDatabaseApplication.class, args);
@@ -21,8 +24,9 @@ public class RedisDatabaseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        redisTemplate.opsForValue().set("spring-r-cluster-1", 123);
-        redisTemplate.opsForValue().set("spring-r-cluster-2", 456);
-        redisTemplate.opsForValue().set("spring-r-cluster-3", 789);
+        log.info("Subiu o servidor redis {}", shoppingDao.findAll());
+        //redisTemplate.opsForValue().set("spring-r-cluster-1", 123);
+        //redisTemplate.opsForValue().set("spring-r-cluster-2", 456);
+        //redisTemplate.opsForValue().set("spring-r-cluster-3", 789);
     }
 }
